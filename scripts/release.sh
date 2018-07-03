7a49e6202d8c1092138afdee6bc714298dce2bf9
spartDev <troux@tillersystems.com>
2018-07-03 11:23:06 +0200

    refactor(scripts): update release scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,58 +2,54 @@
 
 set -e # exit when error
 
-printf "\nReleasing\n"
+printf '\n\e[1;34m%-6s\e[m\n' "Releasing..."
 
 if ! npm owner ls | grep -q "$(npm whoami)"
 then
-  printf "Release: Not an owner of the npm repo, ask a contributor for access"
+  printf '\n\e[1;91m%-6s\e[m\n' "[Release]: Not an owner of the npm repo, ask a contributor for access"
   exit 1
 fi
 
 currentBranch=`git rev-parse --abbrev-ref HEAD`
 if [ $currentBranch != 'master' ]; then
-  printf "Release: You must be on master"
+  printf '\n\e[1;91m%-6s\e[m\n' "[Release]: You must be on Master"
   exit 1
 fi
 
 if [[ -n $(git status --porcelain) ]]; then
-  printf "Release: Working tree is not clean (git status)"
+  printf '\n\e[1;91m%-6s\e[m\n' "[Release]: Working tree is not clean (git status)"
   exit 1
 fi
 
-printf "\n\nRelease: update working tree"
+printf "\n\n[Release]: update working tree"
 git pull origin master
 git fetch origin --tags
 
-printf "Release: yarn"
+printf "[Release]: yarn"
 yarn
 
 currentVersion=`cat package.json | json version`
 
 # header
-printf "\n\nRelease: current version is %s" $currentVersion
-printf "\nRelease: a changelog will be generated only if a fix/feat/performance/breaking token is found in git log"
-printf "\nRelease: you must choose a new ve.rs.ion (semver)"
-printf "\nRelease: press q to exit the next screen\n\n"
+printf "\n\n[Release]: current version is %s" $currentVersion
+printf "\n[Release]: a changelog will be generated only if a fix/feat/performance/breaking token is found in git log"
+printf "\n[Release]: you must choose a new ve.rs.ion (semver)"
+printf "\n[Release]: press q to exit the next screen\n\n"
 
 # preview changelog
-read -p "=> Release: press [ENTER] to view changes since latest version.."
+read -p "=> [Release]: press [ENTER] to view changes since latest version.."
 
 conventional-changelog --preset angular --output-unreleased | less
 
 # choose and bump new version
 # printf "\n\nRelease: Please enter the new chosen version > "
-printf "\n=> Release: please type the new chosen version > "
+printf "\n=> [Release]: please type the new chosen version > "
 read -e newVersion
-
-# regenerate readme TOC
-printf "\n\nRelease: generate TOCS"
-doctoc README.md --maxlevel 3
 
 npm version "$newVersion" --no-git-tag-version
 
 # update changelog
-printf "\n\nRelease: update changelog"
+printf "\n\n[Release]: update changelog"
 conventional-changelog --preset angular --infile CHANGELOG.md --same-file
 
 # git add and tag
@@ -63,14 +59,14 @@ git add package.json CHANGELOG.md README.md
 printf %s "$commitMessage" | git commit --file -
 git tag "v$newVersion"
 
-printf "\n\nRelease: almost done.\n"
+printf "\n\n[Release]: almost done.\n"
 read -p "=> Release: when ready, press [ENTER] to push to github and publish the package"
 
-printf "\n\nRelease: push to github, publish on npm\n"
+printf "\n\n[Release]: push to github, publish on npm\n"
 git push origin master
 git push origin --tags
 
 npm publish
 
-printf "\n\nRelease:
-Package was published to npm.\n\n"
+printf "\n\n[Release]:
+🦄 Package was published to npm.\n\n"
